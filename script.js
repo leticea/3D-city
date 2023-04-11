0a364858172d96d4dca48588330b1f2ff3b81ba8
leticea <nascimento.leticeah@gmail.com>
2023-04-11 10:32:03 -0300

    feat(script.js): main js codes

---
+++ script.js
@@ -0,0 +1,10 @@
+// Base Parameters
+let renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+if (window.innerWidth > 800) {
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+  renderer.shadowMap.needsUpdate = true;
+}
+document.body.appendChild(renderer.domElement);
